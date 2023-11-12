dcbbc3574ff073e01eaef214deb13fc841b6bf2e
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-11-11 20:54:07 -0700

    commit ty

--- src/ThankYou.js
+++ src/ThankYou.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
 const ThankYou = () => {
-  return (
-    <div>
-      <div>
-        <h3>Thank you for donating!</h3>
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <div>
+                <div className='thankyou-card'>
+                    <h3>Thank you for your donation!</h3>
+                    <p>We sent you the receipt at name@email.com!</p>
+                    <img src='/plant.gif' alt='plant' style={{ width: '200px' }} />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ThankYou

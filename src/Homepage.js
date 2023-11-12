4d967077b671b327a9a5f36e85d30dc58ab2d772
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-11-11 23:55:03 -0700

    commit

--- src/Homepage.js
+++ src/Homepage.js
@@ -10,8 +10,8 @@ function Homepage() {
         <img src='/4l.png' alt='leaf' style={{ width: '50px', marginTop: '20px' }} />
       </div>
       <div style={{ width: '90%', marginLeft: '20px', marginTop: "-20px" }}>
-        <h2 className='heading'>Growing Better Places to Live</h2>
-        <p>Tree is the only national non-profit organization dedicated to planting and nurturing trees in rural and urban environments, in every province across the country.</p>
+        <h2 className='heading'>Seeds of Change: Support Tree Growth</h2>
+        <p>Donate now to grow a virtual and real tree in AR, making a lasting impact on our environment. Your contribution makes a tangible difference in fostering a greener world.</p>
       </div>
       <div>
         {/* <h2 style={{ textAlign: 'center' }} className='heading'>Donate To Plant A Tree!</h2> */}

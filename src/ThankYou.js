eb152678cb8e0558bcfbc7ce5e2527d0afd4b840
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-11-11 20:59:09 -0700

    update

--- src/ThankYou.js
+++ src/ThankYou.js
@@ -7,6 +7,9 @@ const ThankYou = () => {
                 <div className='thankyou-card'>
                     <h3>Thank you for your donation!</h3>
                     <p>We sent you the receipt at name@email.com!</p>
+
+                    <p>Your plant number</p>
+                    <p className='plant-number'> 12345</p>
                     <img src='/plant.gif' alt='plant' style={{ width: '200px' }} />
                 </div>
             </div>
